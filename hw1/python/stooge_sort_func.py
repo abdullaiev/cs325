d1253508165392cc997b9f96322d89b46c1a54b2
illia-osu <abdullai@oregonstate.edu>
2018-04-15 21:04:48 -0700

    Fix the stooge sort.

--- hw1/python/stooge_sort_func.py
+++ hw1/python/stooge_sort_func.py
@@ -5,7 +5,7 @@ def stooge_sort(arr, start, end):
         arr[start] = arr[end]
         arr[end] = temp
     elif n > 2:
-        third = (n // 3) + 1
+        third = n // 3
         stooge_sort(arr, start, end - third)
         stooge_sort(arr, start + third, end)
         stooge_sort(arr, start, end - third)
