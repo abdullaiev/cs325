0db3a780ae5b85bcdb1afe45cdde85147c670ab4
illia-osu <abdullai@oregonstate.edu>
2018-05-13 09:52:51 -0700

    HW5 Initial implementation.

---
+++ hw5/README.txt
@@ -0,0 +1,3 @@
+To test the assignment, run `python run.py <input_file_name>`.
+For example, `python run.py in.txt`.
+File in.txt should be in the same directory as run.py file.
---
+++ hw5/run.py
@@ -0,0 +1,126 @@
+import sys
+import os
+
+
+# Parse the input file into a dictionary that represents a graph with wrestlers.
+# Graph's edges represent rivalry between wrestlers.
+def parse():
+    # Open the file specified as the first argument in command line after this file name
+    file_path = os.path.dirname(os.path.realpath(__file__)) + "/" + sys.argv[1]
+    input_file = open(file_path, "r")
+
+    # Read all lines from input file
+    lines = input_file.readlines()
+    loop_count = 0
+    wrestlers_number = 0
+    wrestlers_graph = {}
+
+    # Store a list of connection for later usage
+    connections = []
+
+    for line in lines:
+        # Remove new line characters
+        line = line.strip()
+
+        # The first line contains the number of wrestlers
+        if loop_count == 0:
+            wrestlers_number = int(line)
+
+        # Fill the graph dictionary with wrestler names as keys
+        if 0 < loop_count <= wrestlers_number:
+            if loop_count == 1:
+                start = line
+
+            wrestlers_graph[line] = {
+                'visited': False,
+                'distance': 0,
+                'connections': []
+            }
+
+        # Fill rivalries between wrestlers in the graph dictionary
+        if loop_count > (wrestlers_number + 1):
+            # Split names into array for easy access
+            line = line.split()
+            connections.append(line)
+            wrestlers_graph[line[0]]['connections'].append(line[1])
+            wrestlers_graph[line[1]]['connections'].append(line[0])
+
+        loop_count = loop_count + 1
+
+    return {
+        'start': start,
+        'graph': wrestlers_graph,
+        'connections': connections
+    }
+
+
+# Use BFS algorithm to set the distance for each vertex of the graph from an arbitrary start
+def bfs_distance(graph, start):
+    queue = [start]
+    graph[start]['visited'] = True
+
+    while not len(queue) == 0:
+        wrestler = queue.pop(0)
+        distance = graph[wrestler]['distance'] + 1
+
+        for next_wrestler in graph[wrestler]['connections']:
+            if not graph[next_wrestler]['visited']:
+                graph[next_wrestler]['visited'] = True
+                graph[next_wrestler]['distance'] = distance
+                queue.append(next_wrestler)
+
+    return graph
+
+
+# Checks that every edge goes between an even and odd vertex
+def check_connections(parsed_data):
+    graph = parsed_data['graph']
+    connections = parsed_data['connections']
+
+    for connection in connections:
+        dist1 = graph[connection[0]]['distance']
+        dist2 = graph[connection[1]]['distance']
+        diff = dist2 - dist1
+
+        if diff % 2 == 0:
+            return False
+
+    return True
+
+
+# Forms two teams using the assumption that all vertices with even distance are baby faces
+# and all vertices with odd distance are heels
+def get_teams(graph):
+    babyfaces = "Babyfaces: "
+    heels = "Heels: "
+
+    for wrestler in graph:
+        if graph[wrestler]['distance'] % 2 == 0:
+            babyfaces = babyfaces + wrestler
+            babyfaces = babyfaces + " "
+        else:
+            heels = heels + wrestler
+            heels = heels + " "
+
+    return [babyfaces, heels]
+
+
+# Parse the input file and try to designate all wrestlers
+def run():
+    parsed_data = parse()
+    bfs_distance(parsed_data['graph'], parsed_data['start'])
+
+    # Now every vertex (wrestler) of the graph has a distance from an arbitrary start
+    # Assume that all vertices with even distance are baby faces and all vertices with odd distance are heels
+    # Check that every connection goes between an even and odd wrestler
+    designation_possible = check_connections(parsed_data)
+
+    if designation_possible:
+        print("Yes")
+        teams = get_teams(parsed_data['graph'])
+        print(teams[0])
+        print(teams[1])
+    else:
+        print("No")
+
+run()
